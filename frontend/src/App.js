fd6155c1cd416fa893b7484bfb369a3cbf5e2ff8
ML -GRP - 2024 <164100426+ronaldkalani@users.noreply.github.com>
2024-12-19 12:32:19 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
+export default App;
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DashboardPage from './pages/DashboardPage';
 import TrainingModulePage from './pages/TrainingModulePage';
 
 const App = () => (
   <Router>
-    <Navbar />
-    <div style={{ display: 'flex' }}>
-      <Sidebar />
-      <div style={{ marginLeft: '200px', padding: '20px', flex: 1 }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/training" element={<TrainingModulePage />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/training" element={<TrainingModulePage />} />
+    </Routes>
   </Router>
 );
 
